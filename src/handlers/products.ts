81a029a5f8bf8044b1641a1d9392ab84248cc422
Jesus Elguera Tovar <jesustovaarr@gmail.com>
2025-11-26 17:07:22 -0600

    Implementación de pruebas de integración para el API de productos utilizando Jest y supertest.
    Se crearon pruebas para los endpoints del CRUD de productos ó (/api/products).
    
    Estas pruebas validan tanto los casos de éxito como los de error para las operaciones GET, POST, PUT, PATCH y DELETE.
    
    Las implementaciones incluyen:
    
    POST: Pruebas para la creación de nuevos productos, incluyendo validaciones de errores en la entrada de datos.
    
    GET: Pruebas para obtener la lista completa de productos y para consultar un producto específico por su ID. Se manejan casos de éxito y casos donde el producto no se encuentra.
    
    PUT: Pruebas para la actualización total de un producto. Se valida que se envíen todos los campos requeridos y que los datos sean correctos.
    
    PATCH: Pruebas para la actualización parcial de un producto, específicamente para cambiar su disponibilidad.
    
    DELETE: Pruebas para eliminar un producto, manejando IDs válidos e inválidos, y verificando que el producto ya no exista después de ser eliminado.

--- src/handlers/products.ts
+++ src/handlers/products.ts
@@ -2,40 +2,28 @@ import { Request, Response } from 'express';
 import Product from '../models/Product.model';
 
 export const getProducts = async (req: Request, res: Response) => {
-    try {
-        const products = await Product.findAll({
-            order: [['id', 'DESC']]
-        });
-        res.json({data: products});
-    } catch (error) {
-        res.status(500).json({error: 'Internal Server Error'});
-    }
+    const products = await Product.findAll({
+        order: [
+            ['id', 'DESC']
+        ]
+    });
+    res.json({data: products});
 }
 
 export const getProductsById = async (req: Request, res: Response) => {
-    try {
-        const product =  await Product.findByPk(req.params.id);
+    const product =  await Product.findByPk(req.params.id);
         
-
-        if (!product) {
-            return res.status(404).json({error: 'Product not found'});
-        }
-
-        res.json({data: product});
-    } catch (error) {
-        res.status(500).json({error: 'Internal Server Error'});
+    if (!product) {
+        return res.status(404).json({error: 'Product not found'});
     }
+
+    res.json({data: product});
 }
 
 
 export const createProduct = async (req: Request, res: Response) => {
-    try {
-        const product = await Product.create(req.body);
-        res.status(201).json({data: product});
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({error: 'Internal Server Error'});
-    }
+    const product = await Product.create(req.body);
+    res.status(201).json({data: product});
 }
 
 export const updateProduct = async (req: Request, res: Response) => {
@@ -56,7 +44,6 @@ export const updateProduct = async (req: Request, res: Response) => {
 
 export const updateAvailability = async (req: Request, res: Response) => {
     const product =  await Product.findByPk(req.params.id);
-        
 
         if (!product) {
             return res.status(404).json({error: 'Product not found'});
@@ -69,16 +56,12 @@ export const updateAvailability = async (req: Request, res: Response) => {
 }
 
 export const deleteProduct = async (req: Request, res: Response) => {
-    try {
-        const product =  await Product.findByPk(req.params.id);
-        if (!product) {
-            return res.status(404).json({error: 'Product not found'});
-        }
-
-        await product.destroy();
-
-        res.json({message: 'Product deleted successfully'});
-    } catch (error) {
-        res.status(500).json({error: 'Internal Server Error'});
+    const product =  await Product.findByPk(req.params.id);
+    if (!product) {
+        return res.status(404).json({error: 'Product not found'});
     }
+
+    await product.destroy();
+
+    res.json({message: 'Product deleted successfully'});
 }   
